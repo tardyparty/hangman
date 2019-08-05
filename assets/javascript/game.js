5871098906a14081707925009f466f282bd8784b
tardyparty <tardyparty@users.noreply.github.com>
2019-08-04 21:30:30 -0500

    html and css initial design + psuedocode js

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+var countries = {
+
+}
+
+
+// to do 
+
+// create an array of countries to be guessed in the game 
+
+// capture the users guess 
+
+// compare user guess to word selected
+
+// find random operator to take a random country from array for each game
+
+// display the random word as "_ _ _ _ _"
+
+// reveal correct guesses in display "_ m _ r _ _ _ "
+
+// display wrong guesses "Guesses: q z l b"
+
+// keep track of wins after a correct guess
+
+// display number of guesses remaining and count down
+
+// do not allow user to guess same letter twice
+
+// restart game after user wins or loses
+
